854a17c79c098259e6f24a3dfecb46a650aaf936
igor Danchanka <one2pixel@yandex.ru>
2023-05-16 12:56:48 +0300

    add privacy

--- src/js/files/events.js
+++ src/js/files/events.js
@@ -1,14 +1,17 @@
-
-import { initStartData } from './script.js';
+const welcomeScreen = document.querySelector('.welcome');
 
 // Объявляем слушатель событий "клик"
 
 document.addEventListener('click', (e) => {
 	let targetElement = e.target;
 
-	initStartData();
-
-
+	if (targetElement.closest('.preloader__button')) {
+		document.querySelector('.preloader').classList.add('_hide');
+		sessionStorage.setItem('privacy', true);
+		setTimeout(() => {
+			welcomeScreen.classList.add('_hide');
+		}, 3000);
+	}
 })
 
 
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,12 +1,8 @@
-
 const welcomeScreen = document.querySelector('.welcome');
-
-export function initStartData() {
+if (sessionStorage.getItem('privacy')) {
+	document.querySelector('.preloader').classList.add('_hide');
 	setTimeout(() => {
 		welcomeScreen.classList.add('_hide');
 	}, 3000);
 }
-
-initStartData();
-
 
