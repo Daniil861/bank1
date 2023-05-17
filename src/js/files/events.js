c8f9197044954d6c2ed78609225fbd00e1e3dbe7
igor Danchanka <one2pixel@yandex.ru>
2023-05-17 15:13:02 +0300

    add iframe page

--- src/js/files/events.js
+++ src/js/files/events.js
@@ -8,6 +8,14 @@ document.addEventListener('click', (e) => {
 	if (targetElement.closest('.preloader__button')) {
 		location.href = 'main.html';
 	}
+
+	if (targetElement.closest('[data-btn="show-iframe"]')) {
+		const modal = document.querySelector('.frame-wrapper__modal');
+
+		if (modal && !modal.classList.contains('_visible')) {
+			modal.classList.add('_visible');
+		}
+	}
 })
 
 
