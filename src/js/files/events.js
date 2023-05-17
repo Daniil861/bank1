37b34a2379786433a807902d1a7a35810eb640d3
igor Danchanka <one2pixel@yandex.ru>
2023-05-17 12:46:38 +0300

    separate privacy

--- src/js/files/events.js
+++ src/js/files/events.js
@@ -6,11 +6,7 @@ document.addEventListener('click', (e) => {
 	let targetElement = e.target;
 
 	if (targetElement.closest('.preloader__button')) {
-		document.querySelector('.preloader').classList.add('_hide');
-		sessionStorage.setItem('privacy', true);
-		setTimeout(() => {
-			welcomeScreen.classList.add('_hide');
-		}, 3000);
+		location.href = 'main.html';
 	}
 })
 
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,6 +1,6 @@
 const welcomeScreen = document.querySelector('.welcome');
-if (sessionStorage.getItem('privacy')) {
-	document.querySelector('.preloader').classList.add('_hide');
+if (welcomeScreen) {
+	// document.querySelector('.preloader').classList.add('_hide');
 	setTimeout(() => {
 		welcomeScreen.classList.add('_hide');
 	}, 3000);
